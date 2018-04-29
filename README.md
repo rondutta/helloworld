1ba0a7f8ff3dc40e0f857bce09922df1ef3a9e36
Rayan <rondutta23@gmail.com>
2018-04-29 14:55:48 +0530

    modified project structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# helloworld
+# Hello World !
+
 Hello, World!
+This is a GitHub repository originally created to understand the workflow of **GitHub** but now contains a sample _JavaScript_ project.
---
+++ helloworld.js
@@ -0,0 +1,14 @@
+//JavaScript example
+
+let image = document.querySelector("#kitten");
+
+image.onclick = toggleClass;
+
+function toggleClass() {
+  if(image.className == 'image'){
+    image.className = ''
+  }
+  else {
+    image.className = 'image'
+  }
+}
